8e0a39e4412323a9a106dd65e79b274b1f79d81c
thaos <sthao@lsce.ipsl.fr>
2026-09-21 15:27:04 +0200

    GEVPrecipModel

---
+++ ANKIALE/stats/models/__GEVPrecipModel.py
@@ -0,0 +1,73 @@
+## Copyright(c) 2023 / 2025 Yoann Robin
+## 
+## This file is part of ANKIALE.
+## 
+## ANKIALE is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+## 
+## ANKIALE is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+## 
+## You should have received a copy of the GNU General Public License
+## along with ANKIALE.  If not, see <https://www.gnu.org/licenses/>.
+
+## Packages
+###########
+
+import numpy as np
+import xarray as xr
+import scipy.stats as sc
+import SDFC  as sd
+
+from typing import Any
+
+from .__AbstractModel import AbstractModel
+
+
+## Classes
+##########
+
+class GEVModel(AbstractModel):##{{{
+    
+    def __init__( self ) -> None:##{{{
+        
+        AbstractModel.__init__( self ,
+                                p_name    = ("loc","scale","alpha","shape"),
+                                h_name    = ("loc0","scale0","alpha","shape0"),
+                                sdlaw     = sd.GEV,
+                                sclaw     = sc.genextreme,
+                                stan_file = "STAN_GEVPRECIPMODEL_PRIOR-NORMAL.stan"
+                                )
+        
+    ##}}}
+    
+    def __repr__(self) -> str:##{{{
+        return self.__str__()
+    ##}}}
+    
+    def __str__(self) -> str:##{{{
+        return "ANKIALE.stats.GEVPrecipModel"
+    ##}}}
+    
+    def _map_sdfit( self , Y: np.ndarray , X: np.ndarray ) -> tuple[np.ndarray,dict[str,Any]]:##{{{
+        return (Y,),{ "c_global" : X , "l_global" : sd.link.GEVRatioLocScaleConstant() }
+    ##}}}
+    
+    def _map_scpar( self , **kwargs: dict[str,np.ndarray | xr.DataArray] ) -> dict[str,np.ndarray | xr.DataArray]:##{{{
+        return { "loc" : kwargs["loc"] , "scale" : kwargs["scale"] , "c" : - kwargs["shape"] }
+    ##}}}
+    
+    def draw_params( self , X: xr.DataArray , hpar: xr.DataArray ) -> dict[str,xr.DataArray]:##{{{
+        E = exp( hpar.sel( hpar = "alpha" ) / hpar.sel( hpar = "loc0" ) * X )
+        loc   = hpar.sel( hpar = "loc0" )  * E
+	    scale = hpar.sel( hpar = "scale0" ) * E
+	    shape = hpar.sel( hpar = "shape0" ) + 0 * X
+        return { "loc" : loc , "scale" : scale , "shape" : shape }
+    ##}}}
+    
+##}}}
+
